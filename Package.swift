550d46e5b24e38106cadedae8c925e96089173d4
Jordan Kay <jordan@fleuronic.com>
2024-01-25 19:01:30 -0500

    Add initial fiels

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "GeodeAPI",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6),
+    ],
+    products: [
+        .library(
+            name: "GeodeAPI",
+            targets: ["GeodeAPI"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Fleuronic/GeodeService", branch: "main"),
+        .package(url: "https://github.com/Fleuronic/Caesura", branch: "main"),
+    ],
+    targets: [
+        .target(
+            name: "GeodeAPI",
+            dependencies: [
+                "GeodeService",
+                "Caesura",
+            ]
+        ),
+    ]
+)
