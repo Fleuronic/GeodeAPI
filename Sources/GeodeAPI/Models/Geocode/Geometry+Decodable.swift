7f5359e3f4d4eccb4be8600d2044683a805eeaee
Jordan Kay <jordan@fleuronic.com>
2024-01-26 19:00:27 -0500

    Update models

---
+++ Sources/GeodeAPI/Models/Geocode/Geometry+Decodable.swift
@@ -0,0 +1,19 @@
+import struct Geode.Geocode
+import struct Geode.Location
+
+extension Geocode.Geometry: Decodable {
+	// MARK: Decodable
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		self.init(
+			location: try container.decode(Location.self, forKey: .location)
+		)
+	}
+}
+
+// MARK -
+private extension Geocode {
+	enum CodingKeys: String, CodingKey {
+		case location
+	}
+}
---
+++ Sources/GeodeAPI/Models/Location+Decodable.swift
@@ -0,0 +1,20 @@
+import struct Geode.Location
+
+extension Location: Decodable {
+	// MARK: Decodable
+	public init(from decoder: Decoder) throws {
+		let container = try decoder.container(keyedBy: CodingKeys.self)
+		self.init(
+			latitude: try container.decode(Double.self, forKey: .latitude),
+			longitude: try container.decode(Double.self, forKey: .longitude)
+		)
+	}
+}
+
+// MARK -
+private extension Geocode {
+	enum CodingKeys: String, CodingKey {
+		case latitude = "lat"
+		case longitude = "lng"
+	}
+}
